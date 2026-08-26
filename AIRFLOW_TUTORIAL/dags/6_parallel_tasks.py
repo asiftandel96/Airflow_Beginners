4d70f02bd7cbd0179710ffb2edfe630da3464420
Asif Tandel <asiftandel96@gmail.com>
2026-08-26 13:55:37 +0000

    Parallel Tasks Files

---
+++ AIRFLOW_TUTORIAL/dags/6_parallel_tasks.py
@@ -0,0 +1,61 @@
+from airflow.sdk import dag,task
+
+@dag(dag_id="parallel_tasks_dag")
+
+def parallel_tasks_dag():
+
+    @task.python
+    def extract_task(**kwargs):
+
+        print("Extracting the data")
+
+        ti = kwargs['ti']
+        extracted_data_dict= {"api_extracted_data":[1,2,3],
+                             "db_extracted_data":[4,5,6],
+                             "s3_extracted_data":[7,8,9]}
+    
+        ti.xcom_push(key='return_result',value=extracted_data_dict)
+
+    @task.python
+    def transform_task(**kwargs):
+
+        ti = kwargs['ti']
+
+        db_extracted_data = ti.xcom_pull(task_ids='extract_task',key='return_result')['db_extracted_data']
+        print(f"Transforming the data:{db_extracted_data}")
+
+        transformed_db_data = [i*100 for i in db_extracted_data]
+        ti.xcom_push(key='return_result',value=transformed_db_data)
+
+    @task.python
+    def transform_task_s3(**kwargs):
+
+        ti = kwargs['ti']
+
+        s3_extracted_data = ti.xcom_pull(task_ids='extract_task',key='return_result')['s3_extracted_data']
+        print(f"Transforming the data:{s3_extracted_data}")
+
+        transformed_s3_data = [i*100 for i in s3_extracted_data]
+        ti.xcom_push(key='return_result',value=transformed_s3_data)
+    
+    @task.bash
+    def load_task(**kwargs):
+
+        print("Loading data to destination")
+        api_data = kwargs['ti'].xcom_pull(task_ids='extract_task',key='return_result')['api_extracted_data']
+        db_data = kwargs['ti'].xcom_pull(task_ids='transform_task',key='return_result')
+        s3_data = kwargs['ti'].xcom_pull(task_ids='transform_task_s3',key='return_result')
+
+        return f"echo Loaded Data: {api_data} {db_data} {s3_data}"
+
+    # Defining the task dependencies
+    extract = extract_task()
+    transform_api = transforn_task_api()
+    transform_s3 = transform_task_s3()
+
+    load = load_task()
+
+    extract >> [transform_api, transform_db,transform_s3] >> load
+
+
+parallel_tasks_dag()
